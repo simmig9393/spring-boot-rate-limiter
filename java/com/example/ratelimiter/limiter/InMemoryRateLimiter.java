2264fdc18aef6e1b08117c0467cd7195aa002e49
simmig9393 <simmig.369@gmail.com>
2025-12-17 18:30:08 +0530

    Update InMemoryRateLimiter.java

--- java/com/example/ratelimiter/limiter/InMemoryRateLimiter.java
+++ java/com/example/ratelimiter/limiter/InMemoryRateLimiter.java
@@ -1,3 +1,17 @@
 package com.example.ratelimiter.limiter;
-import java.util.concurrent.*;
-public class InMemoryRateLimiter { private ConcurrentHashMap<String,Integer> map = new ConcurrentHashMap<>(); }
+@Component
+public class InMemoryRateLimiter {
+
+    private final ConcurrentHashMap<String, Bucket> cache = new ConcurrentHashMap<>();
+
+    public boolean allowRequest(String key, int capacity, int refillTokens, int refillSeconds) {
+        Bucket bucket = cache.computeIfAbsent(key, k -> createBucket(capacity, refillTokens, refillSeconds));
+        return bucket.tryConsume(1);
+    }
+
+    private Bucket createBucket(int capacity, int refillTokens, int seconds) {
+        Refill refill = Refill.intervally(refillTokens, Duration.ofSeconds(seconds));
+        Bandwidth limit = Bandwidth.classic(capacity, refill);
+        return Bucket.builder().addLimit(limit).build();
+    }
+}
