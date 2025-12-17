6370dd1decd37c3c31b55dd56054e72b82ee9a66
simmig9393 <simmig.369@gmail.com>
2025-12-17 18:44:29 +0530

    Create RateLimitExceededException.java

---
+++ main/java/src/com/example/ratelimiter/exception/RateLimitExceededException.java
@@ -0,0 +1,11 @@
+package com.example.ratelimiter.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.TOO_MANY_REQUESTS)
+public class RateLimitExceededException extends RuntimeException {
+    public RateLimitExceededException(String m) {
+        super(m);
+    }
+}
